936cbc44213aab1141bb18b47a02b233ddbbbd49
Rosie Liu <rosie.liurx@gmail.com>
2022-03-02 15:24:32 -0500

    initialize

---
+++ app/src/main/java/com/example/simplequizapp/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.simplequizapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
